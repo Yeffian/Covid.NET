85d85a7e6f32f434e1d79bc0aadbaeb1cea7cfa5
YeffyCodeGit <aditchakraborty2009@gmail.com>
2021-12-20 06:59:52 +0600

    added files

---
+++ Covid.NET.Tests/CovidNetTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+using Xunit;
+
+namespace Covid.NET.Tests
+{
+    public class CovidNetTests
+    {
+        [Fact]
+        public void Test_GlobalData()
+        {
+            // idk how to unit test this yet so its blank
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test_CountryData()
+        {
+            // idk how to unit test this yet so its blank
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void Test_AllCountriesData()
+        {
+            // idk how to unit test this yet so its blank
+            Assert.True(true);
+        }
+    }
+}
+
